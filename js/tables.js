1e2ac3d00ab3d1055a34e1fc38d2363d8c4ee803
Vanipriya <vanipriya@gmail.com>
2015-05-26 21:58:06 +0530

    entity

--- js/tables.js
+++ js/tables.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
                 'type': 'article'
             }
         };
-        node_index(query, {
+        entity_index('node',query, {
                 success: function(nodes) {
                 console.log(nodes[1]);
       
