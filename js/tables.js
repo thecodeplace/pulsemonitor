3fe966fe6d35e06c95de66a01c5cea4c955b690d
Vanipriya <vanipriya@gmail.com>
2015-05-26 21:00:58 +0530

    Tables

---
+++ js/tables.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    console.log("Tables");
+    Drupal.settings.site_path = "http://www.thecodeplace.com/pulsemonitor_drup"; // http://www.example.com
+        
+        // Set the Service Resource endpoint path.
+        Drupal.settings.endpoint = "api";
+        
+        // Perform a System Connect call.
+        var query = {
+            parameters: {
+                'type': 'article'
+            }
+        };
+        node_index(query, {
+                success: function(nodes) {
+                console.log(nodes[1].nid);
+      
+            }
+        });
+});
