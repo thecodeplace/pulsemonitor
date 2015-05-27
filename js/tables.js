19723542fc716d1df68c89a7061fd4ab12529c50
Vanipriya <vanipriya@gmail.com>
2015-05-27 09:39:57 +0530

    nodes

--- js/tables.js
+++ js/tables.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         entity_index('entity_node',query, {
                 success: function(nodes) {
                     
-                        console.log(node.length);
+                        console.log(nodes.length);
                     }
       
             
