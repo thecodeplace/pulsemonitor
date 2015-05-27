f60de25c92b29c3a6cd85e3ed292c2146c1b86ca
Vanipriya <vanipriya@gmail.com>
2015-05-27 15:32:36 +0530

    Bar Options

--- js/flot-data.js
+++ js/flot-data.js
@@ -52,6 +52,29 @@ $(document).ready(function() {
                 success: function(nodes) {
                     length = nodes.length;
                     console.log(nodes);
+                    var barOptions = {
+        series: {
+            bars: {
+                show: true,
+                barWidth: 43200000
+            }
+        },
+        xaxis: {
+            mode: "time",
+            timeformat: "%m/%d",
+            minTickSize: [1, "day"]
+        },
+        grid: {
+            hoverable: true
+        },
+        legend: {
+            show: false
+        },
+        tooltip: true,
+        tooltipOpts: {
+            content: "x: %x, y: %y"
+        }
+    };
                     //newdata = new Array(length);
                     for(var i=0;i < nodes.length;i++){
                         var newdata = [nodes[i].BPM,nodes[i].Time];
@@ -1267,29 +1290,7 @@ $(function() {
 
 $(function() {
 
-    var barOptions = {
-        series: {
-            bars: {
-                show: true,
-                barWidth: 43200000
-            }
-        },
-        xaxis: {
-            mode: "time",
-            timeformat: "%m/%d",
-            minTickSize: [1, "day"]
-        },
-        grid: {
-            hoverable: true
-        },
-        legend: {
-            show: false
-        },
-        tooltip: true,
-        tooltipOpts: {
-            content: "x: %x, y: %y"
-        }
-    };
+    
     
 
 });
