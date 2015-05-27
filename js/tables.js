bd3104582c73a789c74ae544b720ba23d744d6fc
Vanipriya <vanipriya@gmail.com>
2015-05-27 10:38:12 +0530

    k

--- js/tables.js
+++ js/tables.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
                     length = nodes.length;
                     newdata = new Array(length);
                     for(var i=0;i < nodes.length;i++){
-                        newdata[i] = String(nodes[i]);
+                        newdata[i] = nodes[i];
                         console(newdata[i]);
                         console.log(nodes[i].field_patient.und[0].value);
                     }
